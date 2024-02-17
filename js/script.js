489ceec674ff68ed4e7222c6ace106dc349ab057
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-02-17 22:25:27 +0500

    added

---
+++ js/script.js
@@ -0,0 +1,223 @@
+const name = document.querySelector('#name');
+const surName = document.querySelector('#surName')
+const age = document.querySelector('#age');
+const phoneNumber = document.querySelector('#phoneNumber')
+const email = document.querySelector('#email');
+const nat = document.getElementById('nationality');
+const desc = document.getElementById('desc');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const button = document.querySelector('#button');
+const wrapper = document.getElementById('wrapper');
+const Delite = document.getElementById('Delite');
+
+function validate(name, age, email, nat, desc, password, surName, phoneNumber) {
+    if (!name.value) {
+        alert('Name kiritish shart');
+        name.focus();
+        name.style.outlineColor = 'red';
+        return false;
+    } else {
+        name.style.outlineColor = 'black'
+    };
+
+    if (name.value.trim().length < 3) {
+        alert('Name kamida 3ta belgidan iborat bolishi shart');
+        name.focus();
+        name.style.outlineColor = 'red';
+        return false;
+
+    } else {
+        name.style.outlineColor = 'black'
+    };
+
+    if (Number(name.value[0])) {
+        alert('Name raqamdan boshlanmaydi');
+        name.focus();
+        name.style.outlineColor = 'red';
+        return false
+    } else {
+        name.style.outlineColor = 'black'
+    };
+
+
+    if (!surName.value) {
+        alert('surname kiritish shart');
+        surName.focus();
+        surName.style.outlineColor = 'red';
+        return false;
+    } else {
+        surName.style.outlineColor = 'black'
+    };
+
+    if (surName.value.trim().length < 5) {
+        alert('surname kamida 3ta belgidan iborat bolishi shart');
+        surName.focus();
+        surName.style.outlineColor = 'red';
+        return false;
+
+    } else {
+        surName.style.outlineColor = 'black'
+    };
+
+    if (Number(surName.value[0])) {
+        alert('Surname raqamdan boshlanmaydi');
+        surName.focus();
+        surName.style.outlineColor = 'red';
+        return false
+    } else {
+        surName.style.outlineColor = 'black'
+    }
+
+
+    if (!age.value) {
+        alert('Age kiritish shart');
+        surName.focus();
+        surName.style.outlineColor = 'red';
+        return false;
+    } else {
+        surName.style.outlineColor = 'black'
+    };
+
+    if (age.value == 0 || age.value > 200) {
+        alert('Age bunday bolmaydi');
+        age.facus();
+        age.style.outlineColor = 'red';
+
+        return false;
+    } else {
+        age.style.outlineColor = 'black'
+    }
+
+    
+    if (!email.value) {
+        alert('email kiritish shart');
+        email.focus();
+        email.style.outlineColor = 'red';
+        return false;
+    } else {
+        email.style.outlineColor = 'black'
+    };
+
+    if (email.email == /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) {
+        alert('email ni boshqalatdan kiriting');
+        email.focus();
+        email.style.outlineColor = 'red';
+    } else {
+        email.style.outlineColor = 'black'
+    };
+
+    if (Number(email.value[0])) {
+        alert('Surname raqamdan boshlanmaydi');
+        email.focus();
+        email.style.outlineColor = 'red';
+        return false
+    } else {
+        email   .style.outlineColor = 'black'
+    };
+
+
+    if (!phoneNumber.value) {
+        alert('phoneNumer kiritish shart');
+        phoneNumber.focus();
+        phoneNumber.style.outlineColor = 'red';
+        return false;
+    } else {
+        phoneNumber.style.outlineColor = 'black'
+    };
+
+    if (phoneNumber.value.substring(0, 3) != '998' ) {
+        alert('phoneNumber (+998) dan boshlanishi shart');
+        phoneNumber.focus();
+        phoneNumber.style.outlineColor = 'red';
+        return false
+    } else {
+        phoneNumber.style.outlineColor = 'black'
+    };
+
+
+
+
+   if (!password.value) {
+        alert('Password kiritish shart');
+        password.focus();
+        password.style.outlineColor = 'red';
+        return false;
+    } else {
+        password.style.outlineColor = 'black'
+    }; 
+
+    return true;
+}
+
+function getData() {
+    let users = [];
+    if (localStorage.getItem('users')) {
+        users = JSON.parse(localStorage.getItem('users'))
+    }
+    return users;
+}
+
+button && button.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+    if (validate(name, age, email, nat, desc, password, surName, phoneNumber)) {
+        const user = {
+            name: name.value,
+            surName: surName.value,
+            age: age.value,
+            phoneNumber: phoneNumber.value,
+            email: email.value,
+            nat: nat.value,
+            desc: desc.value,
+            password: password.value,   
+        }
+
+
+        let u = getData();
+        u.push(user)
+        localStorage.setItem('users', JSON.stringify(u));
+
+
+
+        form.reset();
+
+        let card = createCard(user);
+        wrapper.innerHTML += card;
+
+    } else {
+        console.log('validatsiyadan otmadi');
+    }
+})
+
+function createCard(user) {
+    return `
+    <div class="card">
+            <h3>${user.name}</h3>
+            <h3>${user.surName}</h3>
+            <h3>${user.age}</h3>
+            <h3>${user.phoneNumber}</h3>
+            <h3>${user.email}</h3>
+            <h3>${user.nat}</h3>
+            <h3>${user.desc}</h3>
+            <h3>${user.password}</h3>
+            <button id="Delite">Delite</button>
+        </div>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let users = getData();
+    users.forEach(user => {
+        let card = createCard(user);
+        wrapper.innerHTML += card;
+    });
+})
+
+
+Delite && Delite.addEventListener('click', function() {
+localStorage.clear();
+    wrapper.remove();
+    card.remove()
+})
